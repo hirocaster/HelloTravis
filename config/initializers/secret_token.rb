cc369055e0d36bc23c7dbd8439f340c8016c0de4
hirocaster <hohtsuka@gmail.com>
2012-04-24 15:42:33 +0900

    Init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloTravis::Application.config.secret_token = 'fd07e09031ef04739720d821f342d0ea68681a5bdf546a19ee58fd4c8e021a5e392fa83dfe71769982e7e898fcc0645a897149c66bbf5b8aaaf24caeb2a5ed14'
